23d4a3af9ec48dda76c2a4e667009520c3c180a1
zmil44 <zachary.j.miller@outlook.com>
2018-08-21 14:00:30 -0400

    creaeted buy and sell methods in Trade class

--- SpaceTradingGame/Trade.cs
+++ SpaceTradingGame/Trade.cs
@@ -8,5 +8,27 @@ namespace SpaceTradingGame
 {
     class Trade
     {
+        public string[] buy(string goodName, int quantity)
+        {
+            string[] bought = new string[quantity];
+
+            for (int i = 0; i < quantity; i++)
+            {
+                bought[i] = goodName;
+            }
+
+            return bought;
+        }
+
+        public string[] sell(string goodName, int quantity)
+        {
+            string[] sold = new string[quantity];
+            for (int i = 0; i < quantity; i++)
+            {
+                sold[i] = goodName;
+            }
+
+            return sold;
+        }
     }
 }
