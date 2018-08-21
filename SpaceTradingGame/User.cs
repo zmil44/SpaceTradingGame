08d751102cbb45d20a90a73c26935e8ad0fd0333
zmil44 <zachary.j.miller@outlook.com>
2018-08-21 08:47:07 -0400

    created SetUser method in user class

--- SpaceTradingGame/User.cs
+++ SpaceTradingGame/User.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
 
 namespace SpaceTradingGame
 {
     class User
     {
+        private string _name;
+        private string _shipType;
+        private decimal _credits;
+        private double _time;
+        private decimal _totalCreditsEarned;
+
+
+        private void SetUser(string userName, string userShipType, decimal userCredits, double userTime, decimal userTotalCreditsEarned)
+        {
+            _name = userName;
+            _shipType = userShipType;
+            _credits = userCredits;
+            _time = userTime;
+            _totalCreditsEarned = userTotalCreditsEarned;
+        }
     }
 }
