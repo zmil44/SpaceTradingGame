0d6d088d63a495886ab6fb11982ef84e3398e8c7
zmil44 <zachary.j.miller@outlook.com>
2018-08-21 09:03:48 -0400

    Created method to create a ship

--- SpaceTradingGame/Ship.cs
+++ SpaceTradingGame/Ship.cs
@@ -8,5 +8,16 @@ namespace SpaceTradingGame
 {
     class Ship
     {
+        private string _shipName;
+        private int _shipCargoSpace;
+        private int _shipMaxWarpSpeed;
+        private int _shipCost;
+        private void CreateShip(string name, int cargoSpace, int maxWarpSpeed, int cost)
+        {
+            _shipName = name;
+            _shipCargoSpace = cargoSpace;
+            _shipMaxWarpSpeed = maxWarpSpeed;
+            _shipCost = cost;
+        }
     }
 }
