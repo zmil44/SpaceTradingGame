0f888067dcfed304d70a29847a3fca4ff0da8fc3
zmil44 <zachary.j.miller@outlook.com>
2018-08-21 09:21:52 -0400

    Wrote methods to set and display the story

--- SpaceTradingGame/Story.cs
+++ SpaceTradingGame/Story.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SpaceTradingGame
 {
     class Story
     {
+        private string[] _story;
+        private void setStory()
+        {
+            _story = File.ReadAllLines("SpaceTradingGameStory.txt");
+        }
+
+        private void DisplayStory()
+        {
+            foreach (var word in _story)
+            {
+                Console.Write(word);
+            }
+        }
     }
 }
