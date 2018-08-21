7a51559eecfa1d304417c7568429a3110d38554c
zmil44 <zachary.j.miller@outlook.com>
2018-08-21 12:17:20 -0400

    created UserInterface object in main

--- SpaceTradingGame/SpaceTradingGame.cs
+++ SpaceTradingGame/SpaceTradingGame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,8 @@ namespace SpaceTradingGame
         {
             Console.WindowHeight=Console.LargestWindowHeight;
             Console.WindowWidth = Console.LargestWindowWidth;
-            
+            UserInterface playerInterface = new UserInterface();
+            playerInterface.RunUserInterface();
         }
     }
 }
