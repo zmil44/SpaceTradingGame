bdf149316ed394ff7d4004da0eb14217e8de7f4b
zmil44 <zachary.j.miller@outlook.com>
2018-08-21 08:56:47 -0400

    created get and set methods for the User class

--- SpaceTradingGame/User.cs
+++ SpaceTradingGame/User.cs
@@ -11,9 +11,9 @@ namespace SpaceTradingGame
     {
         private string _name;
         private string _shipType;
-        private decimal _credits;
-        private double _time;
-        private decimal _totalCreditsEarned;
+        private decimal _credits=0;
+        private double _time=0;
+        private decimal _totalCreditsEarned=0;
 
 
         private void SetUser(string userName, string userShipType, decimal userCredits, double userTime, decimal userTotalCreditsEarned)
@@ -23,6 +23,51 @@ namespace SpaceTradingGame
             _credits = userCredits;
             _time = userTime;
             _totalCreditsEarned = userTotalCreditsEarned;
+        }
+
+        private string GetUserName()
+        {
+            return _name;
+        }
+
+        private string GetShipType()
+        {
+            return _shipType;
+        }
+
+        private void SetShipType(string shipType)
+        {
+            _shipType = shipType;
+        }
+
+        private decimal GetCredits()
+        {
+            return _credits;
+        }
+
+        private void SetCredits(decimal credits)
+        {
+            _credits += credits;
+        }
+
+        private double GetUserTime()
+        {
+            return _time;
+        }
+
+        private void SetUserTime(double userTime)
+        {
+            _time = userTime;
+        }
+
+        private decimal GetTotalCreditsEarned()
+        {
+            return _totalCreditsEarned;
+        }
+
+        private void SetTotalCreditsEarned(decimal creditsEarned)
+        {
+            _totalCreditsEarned += creditsEarned;
         }
     }
 }
