2ad7e7847eb73a1c246ab08311a585385a64be20
zmil44 <zachary.j.miller@outlook.com>
2018-08-28 16:03:55 -0400

    removed story file and just had it print to console.

--- SpaceTradingGame/Story.cs
+++ SpaceTradingGame/Story.cs
@@ -9,11 +9,11 @@ namespace SpaceTradingGame
         
         public Story()
         {
-            var story = File.ReadAllLines("SpaceTradingGameStory.txt");
-            foreach (var word in story)
-            {
-                Console.WriteLine(word);
-            }
+            Console.WriteLine("It is the year 2518 and you have just turned 24 years old.After graduating from the Space Pilot Academy," +
+                              "\nyou have decided to travel the stars and trade with people from different planets.Your parents have been " +
+                              "\nsupportive of your decision and have given you a ship and 25,000 credits to start your journey." +
+                              "\nYour ship is a Simiyar - Class Light Frieghter, has a maximum storage of 50 goods, and has a max warp speed of 3." +
+                              "\nYour goal is to earn as many credits as possible after travelling for 40 years.Good luck, and may the force be with you!");
         }
 
     }
