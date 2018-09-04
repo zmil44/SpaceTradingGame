a565df9bf2e948400f69a075eb1e934a2c25f812
zmil44 <zachary.j.miller@outlook.com>
2018-09-04 07:56:10 -0400

    fixed spelling issue

--- SpaceTradingGame/SellMenu.cs
+++ SpaceTradingGame/SellMenu.cs
@@ -41,7 +41,7 @@ namespace SpaceTradingGame
                 return null;
             }
                 Console.WriteLine(
-                    $"You currently have {goodsQuantity[choice]} peices of {goods[choice].GetNameOfGood()}. How many would you like to sell? ");
+                    $"You currently have {goodsQuantity[choice]} pieces of {goods[choice].GetNameOfGood()}. How many would you like to sell? ");
                 int quantity = UserInterface.GetInput();
                 string message = goods[choice].SellGood(player, quantity, choice, goodsQuantity);
                 return message;
