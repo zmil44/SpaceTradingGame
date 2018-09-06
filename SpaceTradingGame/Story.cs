fa2e408d615361ebe123436519f760509ae53dce
zmil44 <zachary.j.miller@outlook.com>
2018-09-06 09:37:24 -0400

    fixed grammer issues in Story

--- SpaceTradingGame/Story.cs
+++ SpaceTradingGame/Story.cs
@@ -9,11 +9,11 @@ namespace SpaceTradingGame
         
         public Story()
         {
-            Console.WriteLine("It is the year 2518 and you have just turned 24 years old.After graduating from the Space Pilot Academy," +
-                              "\nyou have decided to travel the stars and trade with people from different planets.Your parents have been " +
-                              "\nsupportive of your decision and have given you a ship and 25,000 credits to start your journey." +
-                              "\nYour ship is a Simiyar - Class Light Frieghter, has a maximum storage of 50 goods, and has a max warp speed of 3." +
-                              "\nYour goal is to earn as many credits as possible after travelling for 40 years.Good luck, and may the force be with you!");
+            Console.WriteLine("It is the year 2518 and you have just turned 24 years old. After graduating from the Space Pilot Academy, " +
+                              "\nyou have decided to travel the stars and trade with people from different planets. Your parents have been " +
+                              "\nsupportive of your decision and have given you a ship and 25,000 credits to start your journey. " +
+                              "\nYour ship is a Simiyar - Class Light Frieghter, has a maximum storage of 50 goods, and has a max warp speed of 3. " +
+                              "\nYour goal is to earn as many credits as possible after travelling for 40 years. Good luck, and may the force be with you!");
         }
 
         public void DisplayEnd(User player, string exitMessage)
